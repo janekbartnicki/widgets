60604905cf246938b181fd455ba0153c7250ce7e
janekbartnicki <jachu01@poczta.onet.pl>
2022-10-18 18:53:25 +0200

    Building accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Accordion({items}) {
+    const [activeIndex, setActiveIndex] = useState(null);
 
-    const renderedItems = items.map(item => {
+    const onTitleClick = index => setActiveIndex(index);
+
+    const renderedItems = items.map((item, index) => {
+
+        const active = index === activeIndex ? 'active' : '';
+
         return (
-            <div key={item.title}>
-                <div className='title active'>
-                    <i className='dropdown icon'>
-                        {item.title}
-                    </i>
+            <React.Fragment key={item.title}>
+                <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+                    <i className='dropdown icon'></i>
+                    {item.title}
                 </div>
-                <div className='content active'>
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
-            </div>
+            </React.Fragment>
         )
     })
     
